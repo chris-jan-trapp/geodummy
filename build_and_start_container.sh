386089f9de85c77404e725879ee743faf845b36a
chris-jan-trapp <christian.trapp@gbv.de>
2021-03-18 11:35:34 +0100

    fix: Added cd to build script to make it work from everywhere.

--- build_and_start_container.sh
+++ build_and_start_container.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo docker container rm geodummy
+cd /opt/geodummy
 sudo docker build -t "geodummy" .
 sudo cp /var/docker_volumes/geodummy/storage/database.json /var/docker_volumes/geodummy/storage/database.json.bak
 sudo docker run --name geodummy -v /var/docker_volumes/geodummy/storage:/var/storage -p 5000:5000 geodummy
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 scp -r * trapp@esx-80.gbv.de:/opt/geodummy/
-printf 'Now lo onto esx-80 and fire\n\nsudo ./build_and_start_container.sh\n'
+printf 'Now lo onto esx-80 and fire\n\ncd /opt/geodummy && sudo ./build_and_start_container.sh\n'
