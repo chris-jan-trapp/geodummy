ce1eb56a419cfd2529e14aa90faa4efaefefd3a3
Lint Action <lint-action@samuelmeuli.com>
2021-03-24 11:12:23 +0000

    fix: Ran Black

--- src/geodummy.py
+++ src/geodummy.py
@@ -3,33 +3,33 @@ from os import environ
 import json
 
 
-DATA_FILE = environ.get('DATA_FILE', 'tst.json')
-HOST_NAME = environ.get('HOST_NAME', 'local')
+DATA_FILE = environ.get("DATA_FILE", "tst.json")
+HOST_NAME = environ.get("HOST_NAME", "local")
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', HOST_NAME=HOST_NAME)
+    return render_template("index.html", HOST_NAME=HOST_NAME)
 
 
-@app.route('/features/', methods=['GET', 'POST'])
+@app.route("/features/", methods=["GET", "POST"])
 def features(*args, **kwargs):
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.get_json()
-        features = data['features']
-        with open(DATA_FILE, 'r') as data_file:
+        features = data["features"]
+        with open(DATA_FILE, "r") as data_file:
             existing = json.load(data_file)
         existing["features"] += features
-        with open(DATA_FILE, 'w') as data_file:
+        with open(DATA_FILE, "w") as data_file:
             json.dump(existing, data_file)
         return f"Added {len(features)} feature(s)."
 
-    if request.method == 'GET':
-        with open(DATA_FILE, 'r') as data_file:
+    if request.method == "GET":
+        with open(DATA_FILE, "r") as data_file:
             features = json.load(data_file)
         return features
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
