ade032238b858a7fc8dec7cd2fbd496b6d7682fe
chris-jan-trapp <christian.trapp@gbv.de>
2021-03-24 10:29:29 +0100

    chore: Added linter action.

--- build_and_start_container.sh
+++ build_and_start_container.sh
@@ -6,4 +6,4 @@ cd /opt/geodummy
 git pull
 sudo docker build -t "geodummy" .
 sudo cp /var/docker_volumes/geodummy/storage/database.json /var/docker_volumes/geodummy/storage/database.json.bak
-sudo docker run --name geodummy -v /var/docker_volumes/geodummy/storage:/var/storage -p 5000:5000 geodummy
+sudo docker run -d --name geodummy -v /var/docker_volumes/geodummy/storage:/var/storage -p 5000:5000 geodummy
